250e00b8973a516f066868b25c476a4f0a3527a1
Jiwoon <kfromh0136@Naver.com>
2023-11-20 22:16:59 +0900

    [feat] code style setup
    * eslint
    with 'Airbnb Code Style Guide'
    
    * Prettier
    Tab: 2
    Width : 120
    SingleQuote
    Semi
    
    * dependencies
    `peerdeps`
    `eslint-config-airbnb`

--- server/server.js
+++ server/server.js
@@ -4,13 +4,16 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+const User = require('./models/user');
+
 dotenv.config();
 
 const app = express();
 
-mongoose.connect(process.env.DATABASE)
-  .then(() => console.log("Connected to the database"))
-  .catch(err => console.error(err));
+mongoose
+  .connect(process.env.DATABASE)
+  .then(() => console.log('Connected to the database'))
+  .catch((err) => console.error(err));
 
 // Middlewares
 app.use(morgan('dev'));
@@ -18,17 +21,22 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-  res.json("Hello amazon clone");
+  res.json('Hello amazon clone');
 });
 
-app.post("/", (req, res) => {
-  console.log(req.body.name);
-})
+app.post('/', (req, res) => {
+  const user = new User();
+  user.name = req.body.name;
+  user.email = req.body.email;
+  user.password = req.body.password;
 
-app.listen(3000, (err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("Listening on PORT", 3000);
-  }
-})
+  user
+    .save()
+    .then(() => res.json('successfully saved'))
+    .catch((err) => res.json(err));
+});
+
+app
+  .listen()
+  .then(() => console.log('Listening on PORT', 3000))
+  .catch((err) => console.log(err));
