254a0534853d139e7557fc835d9c23e5df32bdd8
Jiwoon <kfromh0136@Naver.com>
2023-12-08 23:09:42 +0900

    [feat] Implement Product Fetching and Display in Vue Component
    Added functionality to fetch products using Axios in the mounted lifecycle hook of the Vue component. The fetched products are stored in the `products` data property. Updated the template to display these products using v-for directive.

--- admin/pages/index.vue
+++ admin/pages/index.vue
@@ -18,16 +18,20 @@
     <div class="a-spacing-large"></div>
     <div class="container-fluid browsing-history">
       <div class="row">
-        <div class="col-xl-2 col-lg-2 col-md-3 col-sm-6 col-6 br bb">
+        <div
+          v-for="(product, index) in products"
+          :key="product._id"
+          class="col-xl-2 col-lg-2 col-md-3 col-sm-6 col-6 br bb"
+        >
           <div class="history-box">
             <!-- Product image -->
             <a herf="#" class="a-link-normal">
-              <img src="#" class="img-fluid" />
+              <img :src="product.photo" class="img-fluid" />
             </a>
             <!-- Product title -->
             <div class="a-spacing-top-base asin-title">
               <span class="a-text-normal">
-                <div class="p13n-sc-truncated">Product title</div>
+                <div class="p13n-sc-truncated">{{ product.title }}</div>
               </span>
             </div>
             <!-- Product rating -->
@@ -45,7 +49,7 @@
             <!-- Product price -->
             <div class="a-row">
               <span class="a-size-base a-color-price">
-                <span class="p13n-sc-price">$23</span>
+                <span class="p13n-sc-price">${{ product.price }}</span>
               </span>
             </div>
             <div class="a-spacing-large"></div>
@@ -63,13 +67,23 @@
 
 <script>
 export default {
-  async data({ $axios }) {
-    try {
-      let response = await $axios.$get('http://localhost:3000/api/products');
-      return {
-        products: products,
-      };
-    } catch (err) {}
+  data() {
+    return {
+      products: [],
+    };
+  },
+  async mounted() {
+    await this.fetchProducts();
+  },
+  methods: {
+    async fetchProducts() {
+      try {
+        let response = await this.$axios.$get('http://localhost:3000/api/products');
+        this.products = response.products;
+      } catch (err) {
+        console.error(err);
+      }
+    },
   },
 };
 </script>
